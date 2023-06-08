63f9e755f714b662b3e0146477cd8811fe236926
Maria Selivanova <maria.m.selivanova@yandex.ru>
2023-06-08 15:48:03 -0300

    feat: migrate controller.ts

---
+++ migration-to-ts/src/components/controller/controller.ts
@@ -0,0 +1,47 @@
+import AppLoader from './appLoader';
+
+class AppController extends AppLoader {
+    public getSources(callback: () => void): void {
+        super.getResp(
+            {
+                endpoint: 'sources',
+            },
+            callback
+        );
+    }
+
+    public getNews(e: Event, callback: () => void): void {
+        let { target } = e;
+        const newsContainer = e.currentTarget;
+
+        if (target instanceof HTMLElement && newsContainer instanceof HTMLElement) {
+            const sourceItem = target.classList.contains('source__item');
+            const sourceId = target.getAttribute('data-source-id');
+            while (target !== newsContainer) {
+                if (target && sourceItem) {
+                    if (typeof sourceId === 'string') {
+                        if (newsContainer.getAttribute('data-source') !== sourceId) {
+                            newsContainer.setAttribute('data-source', sourceId);
+                            super.getResp(
+                                {
+                                    endpoint: 'everything',
+                                    options: {
+                                        sources: sourceId,
+                                    },
+                                },
+                                callback
+                            );
+                        }
+                    }
+                    return;
+                }
+                if (target && target instanceof HTMLElement) {
+                    target = target.parentNode;
+                }
+            }
+        }
+    }
+
+}
+
+export default AppController;
--- migration-to-ts/src/components/controller/loader.ts
+++ migration-to-ts/src/components/controller/loader.ts
@@ -1,17 +1,16 @@
+import { NewsData, SourceData } from "../../types";
+
 interface Options {
- apiKey?: string;
- sources?: string;
- [key: string]: string | undefined;
+    apiKey?: string;
+    sources?: string; 
+    [key: string]: string | undefined;
 }
 
-type HttpMethod = 'GET' | 'POST' | 'PUT' | 'DELETE';
-
-type LoadCallback<T> = (data: T) => void;
 
 class Loader {
-    private baseLink: string;
+    private baseLink: string
 
-    private options: Options;
+    private options: Options
 
     constructor(baseLink: string, options: Options) {
         this.baseLink = baseLink;
@@ -19,27 +18,26 @@ class Loader {
     }
 
    public getResp(
-        { endpoint, options = {} }: {endpoint: string, options: Options},
-        callback = (): void => {
+        { endpoint, options = {} }: { endpoint: string, options?: Options },
+        callback: () => void = (): void => {
             console.error('No callback for GET response');
         }
-    ): void {
+    ):void {
         this.load('GET', endpoint, callback, options);
     }
 
-    private errorHandler = (res: Response): Promise<Response> => {
+   private errorHandler = (res: Response): Response => {
         if (!res.ok) {
-          if (res.status === 401 || res.status === 404) {
-            console.log(`Sorry, but there is ${res.status} error: ${res.statusText}`);
-          }
-          throw Error(res.statusText);
+            if (res.status === 401 || res.status === 404)
+                console.log(`Sorry, but there is ${res.status} error: ${res.statusText}`);
+            throw Error(res.statusText);
         }
-        return Promise.resolve(res);
-      }
 
-  private makeUrl(options: Options, endpoint: string): string {
-        const urlOptions: Options = { ...this.options, ...options };
+        return res;
+    }
 
+   private makeUrl(options: Options, endpoint: string): string {
+        const urlOptions = { ...this.options, ...options };
         let url = `${this.baseLink}${endpoint}?`;
 
         Object.keys(urlOptions).forEach((key) => {
@@ -49,12 +47,12 @@ class Loader {
         return url.slice(0, -1);
     }
 
- private load<T>(method: HttpMethod, endpoint: string, callback: LoadCallback<T>, options: Options):void {
+   private load(method: string, endpoint: string, callback: (data: NewsData | SourceData) => void, options = {}): void {
         fetch(this.makeUrl(options, endpoint), { method })
             .then(this.errorHandler)
             .then((res) => res.json())
-            .then((data: T) => callback(data))
-            .catch((err: Error) => console.error(err));
+            .then((data) => callback(data))
+            .catch((err) => console.error(err));
     }
 }
 
