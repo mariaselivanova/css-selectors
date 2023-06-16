947b9a454d32d5f06b234fc8fb59979da94f5a7d
Maria Selivanova <maria.m.selivanova@yandex.ru>
2023-06-16 11:17:28 -0300

    fix: fix config

--- css-selectors/webpack.config.js
+++ css-selectors/webpack.config.js
@@ -4,45 +4,44 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  entry: { main: './src/index' },
+  entry: { main: './src/index.ts' },
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'main.js',
-    publicPath: ''
+    publicPath: '',
   },
   mode: 'development',
   devServer: {
     static: path.resolve(__dirname, './dist'),
     compress: true,
     port: 8080,
-    open: true
+    open: true,
   },
   module: {
     rules: [
-      {
-        test: /\.ts$/,
-        use: 'babel-loader',
-        exclude: '/node_modules/'
-      },
+      { test: /\.ts$/i, use: 'ts-loader' },
       {
         test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
-        type: 'asset/resource'
+        type: 'asset/resource',
       },
       {
         test: /\.css$/,
         use: [MiniCssExtractPlugin.loader, {
           loader: 'css-loader',
-          options: { importLoaders: 1 }
+          options: { importLoaders: 1 },
         },
-        'postcss-loader']
+        'postcss-loader'],
       },
-      ]
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/index.html'
+      template: './src/index.html',
     }),
     new CleanWebpackPlugin(),
     new MiniCssExtractPlugin(),
-  ]
-}
+  ],
+  resolve: {
+    extensions: ['.ts', '.js', '.json'],
+  },
+};
