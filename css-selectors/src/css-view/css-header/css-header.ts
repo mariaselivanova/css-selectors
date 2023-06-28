5f9d33017945935dcc3069d41550a50007c89f79
Maria Selivanova <maria.m.selivanova@yandex.ru>
2023-06-28 13:53:47 +0300

    feat: add adaptive layout

--- css-selectors/src/css-view/css-header/css-header.ts
+++ css-selectors/src/css-view/css-header/css-header.ts
@@ -1,7 +1,9 @@
 import HeaderView from '../../utils/code-editor-view/header-view/header-view';
+import './css-header.css';
 
 export default class CssHeader extends HeaderView {
   constructor() {
     super('css-editor', 'CSS Editor', 'style.css');
+    this.element?.classList.add('css-header');
   }
 }
--- css-selectors/src/html-view/html-header/html-header.ts
+++ css-selectors/src/html-view/html-header/html-header.ts
@@ -1,7 +1,9 @@
 import HeaderView from '../../utils/code-editor-view/header-view/header-view';
+import './html-header.css';
 
 export default class HtmlHeader extends HeaderView {
   constructor() {
     super('html-editor', 'HTML Viewer', 'index.html');
+    this.element?.classList.add('html-header');
   }
 }
