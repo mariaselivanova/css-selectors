1f22192a48ef95dd45482374d141a7ae3f15cc29
Maria Selivanova <maria.m.selivanova@yandex.ru>
2023-07-19 16:05:18 +0300

    feat: add view class

---
+++ async-race/src/utils/view.ts
@@ -0,0 +1,43 @@
+export default class View {
+  public element: HTMLElement;
+
+  private tag: string;
+
+  constructor(tag: string, classes: string[]) {
+    this.element = View.createElement(tag);
+    this.addClasses(classes);
+    this.tag = tag;
+  }
+
+  private static createElement(tag: string): HTMLElement {
+    return document.createElement(tag);
+  }
+
+  private addClasses(classes: string[]): void {
+    classes.forEach((className) => {
+      if (this.element) {
+        this.element.classList.add(className);
+      }
+    });
+  }
+
+  public setTextContent(text: string):void {
+    if (this.element) {
+      this.element.textContent = text;
+    }
+  }
+
+  public getElement(): HTMLElement {
+    return this.element;
+  }
+
+  public addElements(children: HTMLElement[]):void {
+    children.forEach((child) => this.element?.append(child));
+  }
+
+  public removeContent(): void {
+    while (this.element?.firstChild) {
+      this.element.firstChild.remove();
+    }
+  }
+}
