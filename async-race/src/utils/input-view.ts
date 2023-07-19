9a9fe6c8e9a2390a74807d8f3e45df8a7794f5c6
Maria Selivanova <maria.m.selivanova@yandex.ru>
2023-07-19 21:48:43 +0300

    feat: add input view

---
+++ async-race/src/utils/input-view.ts
@@ -0,0 +1,28 @@
+import View from './view';
+
+export default class InputView extends View {
+  public element: HTMLInputElement | undefined;
+
+  constructor(classes: string[]) {
+    super('input', classes);
+    this.setInputType('text');
+  }
+
+  public setInputType(type: string): void {
+    if (this.element) {
+      this.element.type = type;
+    }
+  }
+
+  public setPlaceholder(text: string): void {
+    if (this.element) {
+      this.element.placeholder = text;
+    }
+  }
+
+  public clearInput(): void {
+    if (this.element) {
+      this.element.value = '';
+    }
+  }
+}
