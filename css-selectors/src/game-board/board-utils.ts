c916f85edf4c40fa494cd828356688b4d649e8af
Maria Selivanova <maria.m.selivanova@yandex.ru>
2023-08-04 02:46:48 +0300

    refactor: update board utils

--- css-selectors/src/game-board/board-utils.ts
+++ css-selectors/src/game-board/board-utils.ts
@@ -2,11 +2,14 @@ import { TagObj } from '../utils/types';
 import View from '../utils/view';
 
 export function createImageDiv(tag: TagObj): {
-  wrapperElement: HTMLElement, imageElement: HTMLElement } {
+  wrapperElement: HTMLElement, imageElement: HTMLElement
+} {
   const wrapper = new View('div', ['image-wrapper']);
   const span = new View('span', ['span']);
   const image = new View('div', [tag.imageClassname, 'image']);
-  span.getElement().textContent = `<${tag.name}${tag.idAttribute ? ` id='${tag.idAttribute}'` : ''}${tag.classAttribute ? ` class='${tag.classAttribute}'` : ''}></${tag.name}>`;
+  const tagId = tag.idAttribute ? ` id='${tag.idAttribute}'` : '';
+  const tagClass = tag.classAttribute ? ` class='${tag.classAttribute}'` : '';
+  span.getElement().textContent = `<${tag.name}${tagId}${tagClass}></${tag.name}>`;
   const wrapperElement = wrapper.getElement();
   const imageElement = image.getElement();
   if (tag.strobe) {
