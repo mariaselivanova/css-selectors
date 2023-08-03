301a27da4d00ec62426d53b3ab9870b2ced2ee75
Maria Selivanova <maria.m.selivanova@yandex.ru>
2023-08-04 01:59:59 +0300

    refactor: update line-counter-view class

--- css-selectors/src/utils/code-editor-view/line-counter-view/line-counter-view.ts
+++ css-selectors/src/utils/code-editor-view/line-counter-view/line-counter-view.ts
@@ -3,39 +3,9 @@ import View from '../../view';
 export default class LineCounterView extends View {
   constructor(className: string) {
     super('div', [className]);
+    const counterLines = [...Array(15).keys()].map((i) => (i < 9 ? `&nbsp;${i + 1}` : i + 1));
     if (this.element) {
-      this.element.innerHTML = `
-      &nbsp;1
-      <br>
-      &nbsp;2
-      <br>
-      &nbsp;3
-      <br>
-      &nbsp;4
-      <br>
-      &nbsp;5
-      <br>
-      &nbsp;6
-      <br>
-      &nbsp;7
-      <br>
-      &nbsp;8
-      <br>
-      &nbsp;9
-      <br>
-      10
-      <br>
-      11
-      <br>
-      12
-      <br>
-      13
-      <br>
-      14
-      <br>
-      15
-      <br>
-      `;
+      this.element.innerHTML = `${counterLines.join('<br>')}`;
     }
   }
 }
